d53ec00d7f1b9e683b6aebeccf058968c37513c0
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-10-11 17:04:24 +0300

    delete unused stuff

--- ts-messageApi/tf/1-lambda-bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "random_pet" "message_lambda_bucket_name" {
-  prefix = "message-lambda"
-  length = 2
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = random_pet.message_lambda_bucket_name.id
-  force_destroy = true
-}
